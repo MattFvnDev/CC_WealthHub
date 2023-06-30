522b01efd073150160dea05e7d3b4311e3df7b99
Mateusz Grzelec <m.grzelec93@gmail.com>
2023-06-30 12:09:46 +0200

    Header and Footer minor styles adjustment.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,38 @@
-import React from "react"
+import React, { useState } from "react"
 import { Icons, Logo, MobileMenu } from "./"
 import { navLinks } from "../constants"
 import { NavLink } from "react-router-dom"
 
-const Header = () => (
-  <header className="sticky top-0 z-10">
-    <nav className="container mx-auto flex justify-between p-6">
-      <Logo />
-      <ul className="hidden items-center md:inline-flex">
-        {navLinks.map((navLink, index) => {
-          return (
-            <li
-              key={navLink.id}
-              className={`cursor-pointer text-[20px] font-bold text-white hover:text-[#d242fd] xl:text-[24px]
+const Header = () => {
+  const [toggle, setToggle] = useState(false)
+  return (
+    <header className="sticky top-0 z-10">
+      <nav className="container mx-auto flex justify-between p-6">
+        <Logo />
+        <ul className="hidden items-center md:inline-flex">
+          {navLinks.map((navLink, index) => {
+            return (
+              <li
+                key={navLink.id}
+                className={`cursor-pointer text-[20px] font-bold text-white hover:text-[#d242fd] xl:text-[24px]
         
                 ${index === navLinks.length - 1 ? `mr-0` : `mr-10`}`}
-            >
-              <NavLink to={`${navLink.path}`}>{navLink.label}</NavLink>
-            </li>
-          )
-        })}
-      </ul>
-      <MobileMenu />
-      <Icons />
-    </nav>
-  </header>
-)
+              >
+                <NavLink
+                  to={`${navLink.path}`}
+                  className="transition delay-75 duration-200 ease-in-out"
+                >
+                  {navLink.label}
+                </NavLink>
+              </li>
+            )
+          })}
+        </ul>
+        <MobileMenu toggle={toggle} setToggle={setToggle} />
+        <Icons />
+      </nav>
+    </header>
+  )
+}
 
 export default Header
--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -1,42 +1,43 @@
-import React, { useState } from "react"
+import React from "react"
 import { CgClose, CgMenuRight } from "react-icons/cg"
 import { navLinks } from "../constants"
 import { NavLink } from "react-router-dom"
 
-const MobileMenu = () => {
-  const [toggle, setToggle] = useState(false)
-
-  return (
+const MobileMenu = ({ toggle, setToggle }) => (
+  <div
+    className="flex items-center md:hidden "
+    onClick={() => setToggle((prev) => !prev)}
+  >
+    {toggle ? (
+      <CgClose className="h-[42px] w-[42px] cursor-pointer stroke-1 text-white transition duration-200 ease-in-out hover:text-[#d242fd]" />
+    ) : (
+      <CgMenuRight className="h-[42px] w-[42px] cursor-pointer stroke-1 text-white transition duration-200 ease-in-out hover:text-[#d242fd]" />
+    )}
     <div
-      className="flex items-center md:hidden"
-      onClick={() => setToggle((prev) => !prev)}
+      className={`${
+        toggle ? `flex` : `hidden`
+      } mobileMenu absolute left-0 right-0 top-16 mx-6 my-4 max-w-3xl justify-center rounded-xl bg-black p-6`}
     >
-      {toggle ? (
-        <CgClose className="h-[42px] w-[42px] cursor-pointer stroke-1 text-white hover:text-[#d242fd]" />
-      ) : (
-        <CgMenuRight className="h-[42px] w-[42px] cursor-pointer stroke-1 text-white hover:text-[#d242fd]" />
-      )}
-      <div
-        className={`${
-          toggle ? `flex` : `hidden`
-        } absolute left-0 right-0 top-16 mx-6 my-2 min-w-[120px] max-w-xl justify-center rounded-xl bg-black p-6`}
-      >
-        <ul className="flex flex-col items-center ">
-          {navLinks.map((navLink, index) => {
-            return (
-              <li
-                key={navLink.id}
-                className={`cursor-pointer text-[28px] font-bold text-white hover:text-[#d242fd]
+      <ul className="flex flex-col items-center ">
+        {navLinks.map((navLink, index) => {
+          return (
+            <li
+              key={navLink.id}
+              className={`cursor-pointer text-[28px] font-bold text-white hover:text-[#d242fd]
                 ${index === navLinks.length - 1 ? `mr-0` : `mb-4`}`}
+            >
+              <NavLink
+                to={`${navLink.path}`}
+                className="transition duration-200 ease-in-out"
               >
-                <NavLink to={`${navLink.path}`}>{navLink.label}</NavLink>
-              </li>
-            )
-          })}
-        </ul>
-      </div>
+                {navLink.label}
+              </NavLink>
+            </li>
+          )
+        })}
+      </ul>
     </div>
-  )
-}
+  </div>
+)
 
 export default MobileMenu
