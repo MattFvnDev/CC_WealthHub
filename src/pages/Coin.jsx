899a79c5093a7e60b94179c41c87bd1a3a0c34da
Mateusz Grzelec <m.grzelec93@gmail.com>
2023-07-07 17:01:55 +0200

    Creating connection to API service coin details endpoint

--- src/pages/Coin.jsx
+++ src/pages/Coin.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React from "react"
+import { useParams } from "react-router-dom"
+import millify from "millify"
+import { useGetCoinDetailsQuery } from "../services/coinsApi"
 
 const Coin = () => {
+  const { coinId } = useParams()
+  const { data, isFetching } = useGetCoinDetailsQuery(coinId)
+  const coinDetails = data?.data?.coin
+  console.log(coinDetails)
+
   return (
-    <div>Coin</div>
+    <div>Coin {coinId}</div>
   )
 }
 
